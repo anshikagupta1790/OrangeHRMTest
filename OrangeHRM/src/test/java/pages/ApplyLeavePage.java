2f1f00b751f914bed54c14606d9a54000a9aafd8
Anshika Gupta <tigerlondon@Parihar-MacBook-Air.local>
2021-03-09 22:49:28 +0100

    updated appleavepage

--- OrangeHRM/src/test/java/pages/ApplyLeavePage.java
+++ OrangeHRM/src/test/java/pages/ApplyLeavePage.java
@@ -43,6 +43,8 @@ public void validateApplyLeave() throws InterruptedException
 		toDate.sendKeys("Fri, 18 Dec 2020");
 		//Thread.sleep(4000);
 		applyleave.submit();
+		System.out.println("Hi");
+		System.out.println("Check");
 		
 	}
 }
